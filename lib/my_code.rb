474372ef7ee1017cb747e48e2919ad27e625a9cb
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-28 02:50:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,17 @@ def map(s)
   new
 end
 
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
+    i += 1
+  end
+  accum
+end
